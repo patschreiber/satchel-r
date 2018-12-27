3ed024c439c62d6cbaac92b54bc698e9a6d69124
Patrick Schreiber <ptos382@gmail.com>
2018-12-27 13:04:16 -0600

    Snapshot

--- src/index.js
+++ src/index.js
@@ -4,56 +4,77 @@ import './index.css';
 import ItemLibrary from './ItemLibrary.js'
 import Satchel from './Satchel.js'
 
-class SatchelRenderer extends React.Component {
+class SatchelContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      satchel: new Satchel(10, 4, ItemLibrary.emptyItem),
-      highlightedSquares: []
+      satchel: new Satchel(10, 4, ItemLibrary.emptyItem)
     }
+
+    this.state.satchel.grid[1][0] = ItemLibrary.testArmor;
+    this.state.satchel.grid[2][0] = ItemLibrary.testArmor;
+    this.state.satchel.grid[3][0] = ItemLibrary.testArmor;
+    this.state.satchel.grid[1][1] = ItemLibrary.testArmor;
+    this.state.satchel.grid[2][1] = ItemLibrary.testArmor;
+    this.state.satchel.grid[3][1] = ItemLibrary.testArmor;
+    
+    this.state.satchel.grid[0][2] = ItemLibrary.skinnySword;
+    this.state.satchel.grid[1][2] = ItemLibrary.skinnySword;
+    this.state.satchel.grid[2][2] = ItemLibrary.skinnySword;
+    this.state.satchel.grid[3][2] = ItemLibrary.skinnySword;
+    
+    this.state.satchel.grid[2][7] = ItemLibrary.testRing;
+    
+    this.state.satchel.grid[0][9] = ItemLibrary.skinnySword;
+    this.state.satchel.grid[1][9] = ItemLibrary.skinnySword;
+    this.state.satchel.grid[2][9] = ItemLibrary.skinnySword;
+    this.state.satchel.grid[3][9] = ItemLibrary.skinnySword;
+    
+    this.state.satchel.grid[0][4] = ItemLibrary.testBook;
+    this.state.satchel.grid[1][4] = ItemLibrary.testBook;
+    this.state.satchel.grid[0][5] = ItemLibrary.testBook;
+    this.state.satchel.grid[1][5] = ItemLibrary.testBook;
+
   }
 
   selectSquare(x, y) {
-    if (this.state.satchel.isClipboardEmpty()) {
-      this.pickContents(x, y);
-    } else {
-      this.putContents(x, y);
-    }
-
-    console.log('item :', item);
-    // satchel.clipboard = ItemLibrary.skinnySword;
-    // this.setState({satchel: satchel })
+    let item = this.state.satchel.grid[y][x];
 
     if (item.itemId !== 0) {
+      let itemCoords = this.state.satchel.findItemCoords(x, y, item.itemId);
       this.setState((previousState) => {
-        previousState.satchel.addToClipboard(item, [[0,0],[0,1]]);
+        console.log('x :', x);
+        console.log('y :', y);
+        //previousState.satchel.addToClipboard(item, itemCoords);
+        previousState.satchel.grid[y][x].status = "red";
         return previousState;
       });
     }
 
-    this.setState({className: "red"})
+    console.log('this.state.satchel :', this.state.satchel);
     console.log('this.state.satchel :', this.state.satchel.isClipboardEmpty());
   }
 
-  pickContents(x, y) {
-    let squareContents = this.state.satchel.grid[y][x];
+  // pickContents(x, y) {
+  //   let squareContents = this.state.satchel.grid[y][x];
 
-    if (squareContents.itemId === 0) {
-      return;
-    }
+  //   if (squareContents.itemId === 0) {
+  //     return;
+  //   }
 
-    let itemCoords = inventory.findItemCoords(x, y, squareContents.itemId);
-    Sizzle("#currently-selected-item")[0].innerHTML = squareContents.name;
+  //   let itemCoords = inventory.findItemCoords(x, y, squareContents.itemId);
+  //   Sizzle("#currently-selected-item")[0].innerHTML = squareContents.name;
 
-    inventory.addToClipboard(squareContents, itemCoords);
-  }
+  //   inventory.addToClipboard(squareContents, itemCoords);
+  // }
 
 
   renderSquare(x, y) {
     return (
       <GridSquare
         onClick={() => this.selectSquare(x, y)}
-        className={this.state.className}
+        className={this.state.satchel.grid[y][x].status}
+        displayText={this.state.satchel.grid[y][x].sym}
       />
     );
   }
@@ -67,6 +88,11 @@ class SatchelRenderer extends React.Component {
           {this.renderSquare(2,0)}
           {this.renderSquare(3,0)}
           {this.renderSquare(4,0)}
+          {this.renderSquare(5,0)}
+          {this.renderSquare(6,0)}
+          {this.renderSquare(7,0)}
+          {this.renderSquare(8,0)}
+          {this.renderSquare(9,0)}
         </div>
         <div className="grid-row">
           {this.renderSquare(0,1)}
@@ -74,6 +100,11 @@ class SatchelRenderer extends React.Component {
           {this.renderSquare(2,1)}
           {this.renderSquare(3,1)}
           {this.renderSquare(4,1)}
+          {this.renderSquare(5,1)}
+          {this.renderSquare(6,1)}
+          {this.renderSquare(7,1)}
+          {this.renderSquare(8,1)}
+          {this.renderSquare(9,1)}
         </div>
         <div className="grid-row">
           {this.renderSquare(0,2)}
@@ -81,6 +112,23 @@ class SatchelRenderer extends React.Component {
           {this.renderSquare(2,2)}
           {this.renderSquare(3,2)}
           {this.renderSquare(4,2)}
+          {this.renderSquare(5,2)}
+          {this.renderSquare(6,2)}
+          {this.renderSquare(7,2)}
+          {this.renderSquare(8,2)}
+          {this.renderSquare(9,2)}
+        </div>
+        <div className="grid-row">
+          {this.renderSquare(0,3)}
+          {this.renderSquare(1,3)}
+          {this.renderSquare(2,3)}
+          {this.renderSquare(3,3)}
+          {this.renderSquare(4,3)}
+          {this.renderSquare(5,3)}
+          {this.renderSquare(6,3)}
+          {this.renderSquare(7,3)}
+          {this.renderSquare(8,3)}
+          {this.renderSquare(9,3)}
         </div>
       </div>
     );
@@ -93,6 +141,7 @@ function GridSquare(props) {
       className={`grid-square ${props.className}`}
       onClick={() => props.onClick()}
     >
+    {props.displayText}
     </div>
   );
 }
@@ -102,7 +151,7 @@ class Inventory extends React.Component {
     return (
       <div className="inventory">
         <div className="satchel">
-          <SatchelRenderer />
+          <SatchelContainer />
         </div>
       </div>
     );
